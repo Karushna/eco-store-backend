767bad86cbf1b64cbb08df6592723068977572c4
Karushna <115612833+Karushna@users.noreply.github.com>
2026-03-26 11:09:55 +0530

    json updated

---
+++ eco-store-backend/controllers/productController.js
@@ -0,0 +1,30 @@
+import Product from "../models/Product.js";
+
+// GET all products
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// POST create product
+export const createProduct = async (req, res) => {
+  try {
+    const { name, price, image, description } = req.body;
+
+    const product = new Product({
+      name,
+      price,
+      image,
+      description,
+    });
+
+    const savedProduct = await product.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
